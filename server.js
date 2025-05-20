2d7d63a69fc4cff711c870892808c3f78576099b
MiroHack0410 <emirbaxin@gmail.com>
2025-05-20 03:33:50 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde ProyectoMasde
-app.use(express.static(path.join(__dirname, 'ProyectoMasde')));
+// Servir archivos estáticos desde la raíz
+app.use(express.static(__dirname));
 
-// Ruta por defecto (index.html)
+// Ruta comodín para redirigir todo a index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'ProyectoMasde', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
