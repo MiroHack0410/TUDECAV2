cbcf528474267b3f5908492ef87b0416e39b4b88
MiroHack0410 <emirbaxin@gmail.com>
2025-05-25 11:42:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { Pool } = require('pg');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const app = express();
